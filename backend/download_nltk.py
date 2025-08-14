f0a61cfb39deb6e4a7853a1befdf6c036a05ef3c
nevinpai <nevinneilpai@gmail.com>
2025-08-13 21:28:09 -0700

    fix nltk

--- backend/download_nltk.py
+++ backend/download_nltk.py
@@ -1,22 +1,5 @@
 import nltk
 
-def download_nltk_data():
-    """
-    Downloads the necessary NLTK data.
-    """
-    try:
-        nltk.data.find('corpora/stopwords')
-    except LookupError:
-        nltk.download('stopwords')
-    try:
-        nltk.data.find('tokenizers/punkt')
-    except LookupError:
-        nltk.download('punkt')
-    try:
-        nltk.data.find('taggers/averaged_perceptron_tagger')
-    except LookupError:
-        nltk.download('averaged_perceptron_tagger')
-
-if __name__ == '__main__':
-    download_nltk_data()
-    print("NLTK data is ready.")
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('averaged_perceptron_tagger')
